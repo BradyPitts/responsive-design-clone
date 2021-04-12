85a2855c57f174b9cdd85cb3d4307e9d05fadbf0
Brady Pitts <this.is.bp67c@gmail.com>
2021-04-12 12:23:59 -0600

    nearly there

--- resposive-design-clone-app/src/App.js
+++ resposive-design-clone-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
 
+
+
 function App() {
   return (
     <div className="App">
--- resposive-design-clone-app/src/Header.js
+++ resposive-design-clone-app/src/Header.js
@@ -1,49 +1,56 @@
-import React from 'react';
-import { Component } from "react";
+import React, { Component} from 'react';
+// import { Component } from "react";
 
-class Header extends Component(){
-  constructor(props){
-    super(props)
-
+class Header extends Component{
+  constructor(){
+    super();
 
     this.state = {
-      open: false
+      menu: 'closed',
+
     }
   }
   
   clickHandler = () => {
-    this.setState({open: !this.state.open})
+    if(this.state.menu === 'closed'){
+      this.setState({menu : 'open'})
+      console.log(`the menu is now ${this.state.menu}`)
+    } 
+    if(this.state.menu === 'open'){
+      this.setState({menu: 'closed'})
+      console.log(`the menu is now ${this.state.menu}`)
+    }
   }
   
 
 
   render(){
-  return (
-    <header>
-      <h3>
-        Start Bootstrap
-      </h3>
-      
-      <nav>
-        <img src='https://i.pinimg.com/564x/ee/c0/71/eec071442e9a1b8e017c5a7c1853b880.jpg' alt='menu' onClick={clickHandler} />
-        <ul className ={open ? 'opened':'closed'}>
-          <li>
-            Services
-          </li>
-          <li>
-            Portfolio
-          </li>
-          <li>
-            Team
-          </li>
-          <li>
-            Contact
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
+    return (
+      <header>
+        <h3>
+          Start Bootstrap
+        </h3>
+        
+        <nav>
+          <img src='https://i.pinimg.com/564x/ee/c0/71/eec071442e9a1b8e017c5a7c1853b880.jpg' alt='menu' onClick= {this.clickHandler} />
+          <ul className ={this.state.menu}>
+            <li>
+              Services
+            </li>
+            <li>
+              Portfolio
+            </li>
+            <li>
+              Team
+            </li>
+            <li>
+              Contact
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }
 
 
